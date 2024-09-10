23ba91291cc3dba80fd859d3654d66705116019b
Gabriel <40807192+SkynoDore@users.noreply.github.com>
2024-09-10 12:51:05 +0200

    Update slider.js
    Fixed error that made duplicated items in the slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,7 +2,6 @@
 function moverDer(){
     var caja1 = document.getElementById('caja1').innerHTML;
     
-    document.getElementById('caja5').innerHTML = document.getElementById('caja1').innerHTML;
     document.getElementById('caja1').innerHTML = document.getElementById('caja2').innerHTML;
     document.getElementById('caja2').innerHTML = document.getElementById('caja3').innerHTML;
     document.getElementById('caja3').innerHTML = document.getElementById('caja4').innerHTML;
